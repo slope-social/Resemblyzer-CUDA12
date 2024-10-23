3fe28b30a6a12df973d247325a75288af789f429
slope-social <128640006+slope-social@users.noreply.github.com>
2024-10-22 21:27:49 -0400

    Update hparams.py

--- resemblyzer/hparams.py
+++ resemblyzer/hparams.py
@@ -1,33 +1,34 @@
+from pathlib import Path
 
-## Mel-filterbank
-mel_window_length = 25  # In milliseconds
-mel_window_step = 10    # In milliseconds
-mel_n_channels = 40
+librispeech_datasets = {
+    "train": {
+        "clean": ["LibriSpeech/train-clean-100", "LibriSpeech/train-clean-360"],
+        "other": ["LibriSpeech/train-other-500"]
+    },
+    "test": {
+        "clean": ["LibriSpeech/test-clean"],
+        "other": ["LibriSpeech/test-other"]
+    },
+    "dev": {
+        "clean": ["LibriSpeech/dev-clean"],
+        "other": ["LibriSpeech/dev-other"]
+    },
+}
 
-
-## Audio
+# Audio settings
 sampling_rate = 16000
-# Number of spectrogram frames in a partial utterance
-partials_n_frames = 160     # 1600 ms
-
-
-## Voice Activation Detection
-# Window size of the VAD. Must be either 10, 20 or 30 milliseconds.
-# This sets the granularity of the VAD. Should not need to be changed.
-vad_window_length = 30  # In milliseconds
-# Number of frames to average together when performing the moving average smoothing.
-# The larger this value, the larger the VAD variations must be to not get smoothed out. 
+audio_norm_target_dBFS = -30
+vad_window_length = 30
 vad_moving_average_width = 8
-# Maximum number of consecutive silent frames a segment can have.
 vad_max_silence_length = 6
 
+# Mel-filterbank settings
+mel_window_length = 25
+mel_window_step = 10
+mel_n_channels = 40
 
-## Audio volume normalization
-audio_norm_target_dBFS = -30
-
-
-## Model parameters
+# Model parameters
+partials_n_frames = 160
 model_hidden_size = 256
 model_embedding_size = 256
 model_num_layers = 3
-
