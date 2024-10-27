b5e1f3350e3dbbd4c69d65d38a3b58b7a0c882d3
slope-social <128640006+slope-social@users.noreply.github.com>
2024-10-27 06:42:36 -0400

    Update setup.py | update version to 0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="resemblyzer",
-    version="0.2.3",
+    version="0.2.4",  # Incrementing version for CUDA 12.1 nightly support
     description="Real Time Voice Cloning with CUDA 12 Support",
     url="https://github.com/slope-social/Resemblyzer-CUDA12",
     author="Slope Social",
@@ -13,13 +13,23 @@ setup(
     },
     python_requires=">=3.10",
     install_requires=[
-        "torch==2.4.1",
-        "torchaudio==2.4.1",
+        "torch==2.6.0.dev20241026+cu121",
+        "torchaudio==2.5.0.dev20241026+cu121",
         "librosa>=0.10.1",
         "numpy>=1.23.5",
         "webrtcvad>=2.0.10",
         "scipy>=1.14.1",
         "typing_extensions>=4.12.0"
     ],
-    include_package_data=True
+    dependency_links=[
+        "https://download.pytorch.org/whl/nightly/cu121"
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Multimedia :: Sound/Audio :: Speech"
+    ]
 )
