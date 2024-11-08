2d7d9d1d61e6c67e77d29d92b1d9e7e05104f452
slope-social <128640006+slope-social@users.noreply.github.com>
2024-11-08 13:31:54 -0500

    Update __init__.py

--- resemblyzer/__init__.py
+++ resemblyzer/__init__.py
@@ -1,6 +1,16 @@
+# resemblyzer/__init__.py
 name = "resemblyzer"
 
-from resemblyzer.audio import preprocess_wav, wav_to_mel_spectrogram, trim_long_silences, \
-    normalize_volume
+from resemblyzer.audio import preprocess_wav, wav_to_mel_spectrogram, trim_long_silences, normalize_volume
 from resemblyzer.hparams import sampling_rate
-from resemblyzer.voice_encoder import VoiceEncoder
+from resemblyzer.voice_encoder import VoiceEncoder, CUDAVoiceEncoder
+
+__all__ = [
+    "VoiceEncoder",
+    "CUDAVoiceEncoder",
+    "preprocess_wav",
+    "wav_to_mel_spectrogram",
+    "trim_long_silences",
+    "normalize_volume",
+    "sampling_rate"
+]
